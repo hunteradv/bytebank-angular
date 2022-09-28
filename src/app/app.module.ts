8df23f33b75060e2eb831900d83b3480352864f8
HunterAdv <gustavopaulody@gmail.com>
2022-09-28 20:29:44 -0300

    - Importando FormsModule em app.module. - inserindo ngModel no formulário. - inserindo novas properties e método em nova-transferencia-component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     NovaTransferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,18 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-@Component ({
+@Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-
 export class NovaTransferenciaComponent {
 
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
+  }
 }
